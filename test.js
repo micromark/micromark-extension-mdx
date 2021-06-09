1512ce0bfe65f3a74aeeffc964a9559ed9e46226
Titus Wormer <tituswormer@gmail.com>
2021-06-09 15:58:45 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'tape'
 import {micromark} from 'micromark'
 import {mdx} from './index.js'
 
-test('markdown -> html (micromark)', function (t) {
+test('markdown -> html (micromark)', (t) => {
   t.deepEqual(
     micromark('<div>\n{asd}\n</div>', {extensions: [mdx()]}),
     '\n\n',
