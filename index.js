e50903574fbf7f7c1371cb2332f35cfc7c18942f
Titus Wormer <tituswormer@gmail.com>
2021-06-09 16:04:54 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+/**
+ * @typedef {import('micromark-util-types').Extension} Extension
+ */
+
 import {combineExtensions} from 'micromark-util-combine-extensions'
+// @ts-expect-error: next
 import expression from 'micromark-extension-mdx-expression'
+// @ts-expect-error: next
 import jsx from 'micromark-extension-mdx-jsx'
+// @ts-expect-error: next
 import md from 'micromark-extension-mdx-md'
 
+/**
+ * @returns {Extension}
+ */
 export function mdx() {
+  // @ts-expect-error: It’s fine.
   return combineExtensions([expression(), jsx(), md])
 }
