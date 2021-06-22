da9dd6ad469fd5478b2dc2aefbcc40bcf4e5100c
Titus Wormer <tituswormer@gmail.com>
2021-06-22 22:22:09 +0200

    Update `micromark`

--- index.js
+++ index.js
@@ -3,17 +3,13 @@
  */
 
 import {combineExtensions} from 'micromark-util-combine-extensions'
-// @ts-expect-error: next
-import expression from 'micromark-extension-mdx-expression'
-// @ts-expect-error: next
-import jsx from 'micromark-extension-mdx-jsx'
-// @ts-expect-error: next
-import md from 'micromark-extension-mdx-md'
+import {mdxExpression} from 'micromark-extension-mdx-expression'
+import {mdxJsx} from 'micromark-extension-mdx-jsx'
+import {mdxMd} from 'micromark-extension-mdx-md'
 
 /**
  * @returns {Extension}
  */
 export function mdx() {
-  // @ts-expect-error: It’s fine.
-  return combineExtensions([expression(), jsx(), md])
+  return combineExtensions([mdxExpression(), mdxJsx(), mdxMd])
 }
