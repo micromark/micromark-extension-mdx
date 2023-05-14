279564f76c2d89809e0f047ef5a33c080ad0ffa6
Titus Wormer <tituswormer@gmail.com>
2023-05-14 16:10:26 +0200

    Add tests for exposed identifiers

--- test.js
+++ test.js
@@ -4,6 +4,13 @@ import {micromark} from 'micromark'
 import {mdx} from './index.js'
 
 test('markdown -> html (micromark)', async function () {
+  assert.deepEqual(
+    // To do: next major: use export map, use package name here.
+    Object.keys(await import('./index.js')).sort(),
+    ['mdx'],
+    'should expose the public api'
+  )
+
   assert.equal(
     micromark('<div>\n{asd}\n</div>', {extensions: [mdx()]}),
     '\n\n',
