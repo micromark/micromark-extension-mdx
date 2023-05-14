f4739268a017253c3f49eb0b366a64e7ed127d75
Titus Wormer <tituswormer@gmail.com>
2023-05-14 16:07:56 +0200

    Refactor code-style

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
  * @typedef {import('micromark-util-types').Extension} Extension
  */
 
-import {combineExtensions} from 'micromark-util-combine-extensions'
 import {mdxExpression} from 'micromark-extension-mdx-expression'
 import {mdxJsx} from 'micromark-extension-mdx-jsx'
 import {mdxMd} from 'micromark-extension-mdx-md'
+import {combineExtensions} from 'micromark-util-combine-extensions'
 
 /**
+ * Create an extension for `micromark` to enable MDX syntax, unaware of
+ * JavaScript.
+ *
  * @returns {Extension}
+ *   Extension for `micromark` that can be passed in `extensions` to enable MDX
+ *   syntax, unaware of JavaScript.
  */
 export function mdx() {
   return combineExtensions([mdxExpression(), mdxJsx(), mdxMd])
