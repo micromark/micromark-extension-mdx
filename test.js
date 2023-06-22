500cc9d4cb67422dd2bd15d231940e3ee3c301d7
Titus Wormer <tituswormer@gmail.com>
2023-06-22 17:08:11 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -3,17 +3,19 @@ import test from 'node:test'
 import {micromark} from 'micromark'
 import {mdx} from './index.js'
 
-test('markdown -> html (micromark)', async function () {
-  assert.deepEqual(
-    // To do: next major: use export map, use package name here.
-    Object.keys(await import('./index.js')).sort(),
-    ['mdx'],
-    'should expose the public api'
-  )
+test('markdown -> html (micromark)', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(
+      // To do: next major: use export map, use package name here.
+      Object.keys(await import('./index.js')).sort(),
+      ['mdx']
+    )
+  })
 
-  assert.equal(
-    micromark('<div>\n{asd}\n</div>', {extensions: [mdx()]}),
-    '\n\n',
-    'should work'
-  )
+  await t.test('should work', async function () {
+    assert.equal(
+      micromark('<div>\n{asd}\n</div>', {extensions: [mdx()]}),
+      '\n\n'
+    )
+  })
 })
